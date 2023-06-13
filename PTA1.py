4a45e2e10685cb626405dae71738a08a4596cdcf
SwS_Kz <59721856+ben12345448@users.noreply.github.com>
2023-06-13 13:23:41 +0800

    Add files via upload

---
+++ PTA1.py
@@ -0,0 +1,25 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Login As：', client.user)
+    game = discord.Game('lol')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "test" or message.content == "Test":
+        await message.channel.send('Hi')
+
+    if message.content.startswith('!'):
+      tmp = message.content.split(" ",2)
+      if len(tmp) == 1:
+        await message.channel.send("?")
+      else:
+        await message.channel.send(tmp[1])
+
+client.run("MTExODAyMDQ0NDYwNDM1MDQ5NQ.GcoBmC.I9_WAX2RUhc4_EKJF7P9Y9iD7eEZvq9To-lvqU")  
